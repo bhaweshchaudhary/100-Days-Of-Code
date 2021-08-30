a2a8fb49186a8e0c62bf138eab6da3a56f3f6e07
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-08-30 21:52:59 +0530

    modification on day 1

---
+++ Day1/Day1.py
@@ -0,0 +1,20 @@
+# Print statement
+print("Bhawesh Chaudhary")
+
+# string concatenation
+
+print("Bhawesh " + "Chaudhary")
+print("Bhawesh" + " " + "Chaudhary")
+
+#debugging the code below
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with '+' sign.")
+print("e.g. print('Hello ' + 'world') ")
+print("New line can be created with a backslash and n.")
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 100DaysOfCodeInPython
+
+## Day 1
+Printing, Commenting, Debugging, String Manipulation and Variables.
+Project - Band Name Generator
+Errors Encountered - Syntax Error, Indentation Error
