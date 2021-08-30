1ac09b9810db53ae3cc4268719f163f4dbd6c521
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-08-30 22:43:54 +0530

    exercise 3 added

---
+++ Day1/exercise3.py
@@ -0,0 +1,15 @@
+# write code to inter change the value of variables
+
+numberOne = input("Enter any number\n")
+numberTwo = input("Enter another any number\n")
+
+# Swapping values here
+
+numberThree = numberOne
+numberOne = numberTwo
+numberTwo = numberThree
+
+# Printing the swapped values of variables 
+
+print("Number One equals to " + numberOne)
+print("Number Two equals to " + numberTwo)
