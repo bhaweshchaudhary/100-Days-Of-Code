f647e68bcd8f4b2d6cbcb68c8fb6c24513a224e2
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-08-30 22:26:35 +0530

    Files Updated

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -11,3 +11,13 @@ name = input("what is your name?\n")
 print("My name is " + name)
 
 print("Hello " + input("What is your name?\n"))
+
+# variables
+
+myFirstName = input("Please enter your first name\n")
+myLastName = input("please enter your last name\n")
+
+myFullName = myFirstName + " " + myLastName
+print("My full name is " + myFullName)
+print("Below is the length of your full name\n")
+print(len(myFullName))
