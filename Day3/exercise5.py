51c5d0767d86b62694ef507f21a252443b59e218
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-09-06 20:08:53 +0530

    completed exercise 5

--- Day3/exercise5.py
+++ Day3/exercise5.py
@@ -1,13 +1,27 @@
 # Don't change the code below
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? S, M, or L ").upper()
+add_pepperoni = input("Do you want pepperoni? Y or N ").upper()
+extra_cheese = input("Do you want extra cheese? Y or N ").upper()
 # Don't change the code above 
 
 #Write your code below this line
+bill = 0
+if size == "S":
+    bill = 15
+    if add_pepperoni == "Y":
+        bill += 3
+if size == "M":
+    bill = 20
+    if add_pepperoni == "Y":
+        bill += 3
+if size == "L":
+    bill = 25
+    if add_pepperoni == "Y":
+        bill += 3
 
-
-
+if extra_cheese == "Y":
+    bill += 1
+print(f"Your final bill is ${bill}")
 
 
