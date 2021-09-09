100864087b7a249665a849c7bfa1e61d5c0dcc1d
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-09-09 23:13:07 +0530

    love calculator

---
+++ Day3/exercise6.py
@@ -0,0 +1,9 @@
+# Don't change the code below
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# Don't change the code above
+
+#Write your code below this line
+
+
