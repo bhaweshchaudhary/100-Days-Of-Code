8d16d5f026e770f428f803c751b336ffae763faa
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-09-11 19:10:34 +0530

    love calculator completed

--- Day3/exercise6.py
+++ Day3/exercise6.py
@@ -7,3 +7,24 @@ name2 = input("What is their name? \n")
 #Write your code below this line
 
 
+combined_name = name1 + name2
+
+small_name = combined_name.lower()
+
+t = small_name.count("t")
+r = small_name.count("r")
+u = small_name.count("u")
+e = small_name.count("e")
+
+true = t + r + u + e
+
+l = small_name.count("l")
+o = small_name.count("o")
+v = small_name.count("v")
+e = small_name.count("e")
+
+love = l + o + v + e
+
+true_love = str(true) + str(love)
+
+print(true_love)
