16ac2d26cb4efec4ebbf62f9464988dbc83dbcbf
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-09-15 00:15:24 +0530

    applying random function with integers

--- Day4/main.py
+++ Day4/main.py
@@ -4,6 +4,11 @@ import my_module
 random_numbers = random.randint(100, 200)
 
 pi = my_module.pi
+
 # this random() generates number between 0.0 to 1.0 but not including 1.0 for eg. it can go like 0.9999 but not 1.0
 random_float = random.random()
-print(random_float)
+
+# multiply random function with integers
+# we get floating numbers from 0.0 to 5.0 but not including 5.0
+new_random = random_float * 5
+print(new_random)
