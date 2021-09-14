878e7ccf7e504488c7aef6b7d775880c96a87c2a
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-09-14 23:05:22 +0530

    random

--- Day4/main.py
+++ Day4/main.py
@@ -4,4 +4,6 @@ import my_module
 random_numbers = random.randint(100, 200)
 
 pi = my_module.pi
-print(pi)
+# this random() generates number between 0.0 to 1.0 but not including 1.0 for eg. it can go like 0.9999 but not 1.0
+random_float = random.random()
+print(random_float)
