97cd553b11a41f21b62fd956242d3ac07084a859
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-09-25 03:44:57 +0530

    next exercise

---
+++ Day4/who_will_pay.py
@@ -0,0 +1,9 @@
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# Don't change the code above
+
+#Write your code below this line 
+
+
+
