89412e5f1d1b910fb0f8dc94a8d9f4eb8906a361
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-09-25 04:19:24 +0530

    updated

---
+++ Day4/tresure_map.py
@@ -0,0 +1,20 @@
+# Don't change the code below 
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# Don't change the code above 
+
+#Write your code below this row 
+
+
+
+
+
+
+#Write your code above this row 
+
+# Don't change the code below 
+print(f"{row1}\n{row2}\n{row3}")
