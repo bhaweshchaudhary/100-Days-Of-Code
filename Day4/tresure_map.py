e438092eb24f7a5f30191f3f16d2230723ce9e31
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-10-02 09:21:13 +0530

    completed tresure map

--- Day4/tresure_map.py
+++ Day4/tresure_map.py
@@ -1,7 +1,7 @@
 # Don't change the code below 
-row1 = ["⬜️","⬜️","⬜️"]
-row2 = ["⬜️","⬜️","⬜️"]
-row3 = ["⬜️","⬜️","⬜️"]
+row1 = ["🙂","🙂","🙂"]
+row2 = ["🙂","🙂","🙂"]
+row3 = ["🙂","🙂","🙂"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
@@ -9,12 +9,16 @@ position = input("Where do you want to put the treasure? ")
 
 #Write your code below this row 
 
+try:
+    horizontal = int(position[0])
+    vertical = int(position[1])
 
+    selected_row = (map[vertical-1])
+    selected_row[horizontal-1] = "🤑"
 
-
+    print(f"{row1}\n{row2}\n{row3}")
+except:
+    print("Enter less than or equal to 33")
 
 
 #Write your code above this row 
-
-# Don't change the code below 
-print(f"{row1}\n{row2}\n{row3}")
