a08b56296a520fde882798749df27470c10e44c3
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-10-06 23:27:23 +0530

    code updated

--- Day4/rock_paper_scissor.py
+++ Day4/rock_paper_scissor.py
@@ -32,15 +32,12 @@ game_images = [rock, paper, scissors]
 # if (int(user_choice) < 0 or int(user_choice) >= 3):
 
 try:
-
     user_choice = int(input("Choose between rock (press 0), paper (press 1), and scissor(press 2)\n"))
-    # print("You chose:\n")
     print(f"You chose \n {game_images[int(user_choice)]}")
 
     computer_choice = random.randint(0,2)
     print(f"Computer chose \n {game_images[computer_choice]}")
 except:
-    
     print("Please enter a valid number.")
 
 
