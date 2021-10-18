52bdd9a1b1f1ba497609c05e6f49542f7aa96d34
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-10-18 16:43:57 +0530

    new exercise added

--- Day5/high_score.py
+++ Day5/high_score.py
@@ -1,1 +1,12 @@
-import os
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+
+
+
