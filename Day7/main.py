4ec63bad21e807bb1e10e45fdd25062d2a33fe6e
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-11-03 15:41:09 +0530

    hangman

--- Day7/main.py
+++ Day7/main.py
@@ -2,19 +2,84 @@ word_list = ["ardvark", "baboon", "camel"]
 
 # 1st - Randomly chose word from word_list and assign it to the variable called chosen_word
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 chosen_word = random.choice(word_list)
 print(chosen_word)
+listing = []
+for blank in chosen_word:
+    listing += "_"
+print(listing)
 
 # 2nd - Ask the user to guess the letter and assign their answer to the variable called guess. Make guess lowercase.
+end_of_game = False
+while not end_of_game:
+    guess = input("Please guess the letter\n").lower()
 
-guess = input("Please guess the letter\n").lower()
+    # 3rd - Check if the letter the user guessed (guess) is one of the letters in chosen word
 
-# 3rd - Check if the letter the user guessed (guess) is one of the letters in chosen word
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            listing[position] = letter
+    print(listing)
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("wrong")
-
-# 6. challenge 2 solution -  how to replace the blank
+    if "_" not in listing:
+        end_of_game = True
+        print("You win")
