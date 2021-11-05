89574876b3fa441d9031e0b8757210b9bcc4b1fa
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-11-05 16:24:26 +0530

    day 8 learning

---
+++ Day8/main.py
@@ -0,0 +1,11 @@
+# simple functions
+def greeting():
+    print("Hello world")
+
+greeting()
+
+# function with inputs
+def greet(name): # here name in parenthesis is parameter
+    print(f"Hello {name}")
+
+greet("world") # here world is argument
