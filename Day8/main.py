0672ff9860ba6f7e158e49f67b264d5c0c7a26a9
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-11-05 16:30:06 +0530

    positional arguments

--- Day8/main.py
+++ Day8/main.py
@@ -9,3 +9,14 @@ def greet(name): # here name in parenthesis is parameter
     print(f"Hello {name}")
 
 greet("world") # here world is argument
+
+
+# positional vs key arguments
+
+# functions with more than one inputs
+
+def greet_with(name, location):
+    print(f"Hello {name}\n")
+    print(f"What is it like in {location}")
+
+greet_with("world", "vizag") # arguments are positional arguments because it supply the value to the parameters at same location / position and it default.
