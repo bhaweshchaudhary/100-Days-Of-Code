24a0e5e902105a2557a786c5b50ba4fbed06e63a
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-11-05 16:35:06 +0530

    keyword arguments

--- Day8/main.py
+++ Day8/main.py
@@ -14,9 +14,18 @@ greet("world") # here world is argument
 # positional vs key arguments
 
 # functions with more than one inputs
+# positional arguments
 
 def greet_with(name, location):
     print(f"Hello {name}\n")
     print(f"What is it like in {location}")
 
 greet_with("world", "vizag") # arguments are positional arguments because it supply the value to the parameters at same location / position and it default.
+
+# keyword arguments
+
+def greet_with(name, location):
+    print(f"Hello {name}\n")
+    print(f"What is it like in {location}")
+
+greet_with(location="vizag", name="bhawesh") # in keyword arguments we specify which arguments is the value of which parameters.
