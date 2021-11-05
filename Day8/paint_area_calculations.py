4e64268b34a18cf8fe5df906a1e67d6a1fb07e2f
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-11-05 19:15:46 +0530

    paint area calculation

---
+++ Day8/paint_area_calculations.py
@@ -0,0 +1,19 @@
+#Write your code below this line 👇
+
+
+def paint_calc(height, width, cover):
+    calc = (height*width)/cover
+    print(calc)
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
