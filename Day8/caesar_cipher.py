1e5b6d966659db0335ebc6918f478d967675bc1f
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-11-06 08:41:41 +0530

    caesar cipher

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -1,5 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
@@ -22,6 +23,7 @@ def decrypt(cipher_text, shift_amount):
         new_position = position - shift_amount
         plain_text += alphabet[new_position]
     print(f"The decoded text is {plain_text}")
+
 
 if direction == "encode":
     encrypt(plain_text=text, shift_amount=shift)
