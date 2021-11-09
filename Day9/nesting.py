286df0f224e043ee3b2a7fff6019661a0545557d
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-11-09 21:34:04 +0530

    nesting a list in a dictionary

---
+++ Day9/nesting.py
@@ -0,0 +1,12 @@
+# Nesting a List in a Dictionary
+
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 20,
+    },
+}
