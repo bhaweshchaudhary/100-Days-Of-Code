8a4e90382097d22d1593f54b9608f5f4ea5e5ec8
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-11-13 20:43:43 +0530

    new exercise added

--- Day9/nesting.py
+++ Day9/nesting.py
@@ -10,3 +10,10 @@ travel_log = {
         "total_visits": 20,
     },
 }
+
+# Nesting a dictionary in a list
+
+travel_log = [
+        {"country": "France", "cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+        {"country": "Germany", "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 20},
+]
