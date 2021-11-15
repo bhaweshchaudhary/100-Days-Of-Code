840a84eb390418679d4fa0bb07f5d576a752109d
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-11-15 23:42:22 +0530

    more functions

--- Day10/more_functions.py
+++ Day10/more_functions.py
@@ -1,7 +1,14 @@
-# function with output
+# # function with output
 
-def my_function():
-    result = 4*2
-    return result
+# def my_function():
+#     result = 4*2
+#     return result
 
-my_function()
+# my_function()
+
+def format_name(f_name, l_name):
+    full_name = f_name + " " + l_name
+    title_case_full_name = full_name.title()
+    print(title_case_full_name)
+
+format_name(f_name=input("What's your first name?\n"), l_name=input("What's your last name?\n"))
