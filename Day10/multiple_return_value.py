0e317492e2657c01de88454cb386b47fee94f042
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-11-16 16:33:50 +0530

    added extra return in function

--- Day10/multiple_return_value.py
+++ Day10/multiple_return_value.py
@@ -1,4 +1,6 @@
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide the valid input.\n"
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
     return f"{formatted_f_name} {formatted_l_name}"
