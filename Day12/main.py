d1ae6c56535dd59c1d4623bf4f85a0a136a2a654
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-12-08 22:15:49 +0530

    local and global scope

--- Day12/main.py
+++ Day12/main.py
@@ -8,3 +8,19 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function {enemies}.")
+
+# Local scope
+def hello():
+    world = 1
+    print(world)
+hello()
+
+# Global scope
+
+hello_world = 4
+def hellow():
+    print(hello_world)
+hellow()
+print(hello_world)
+
+# 2nd video: does python have block scope
