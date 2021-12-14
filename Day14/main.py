eaaf985983a4c4892cdae2944aca1ad669a2819a
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-12-14 18:26:37 +0530

    Day 14 Projects initialized

---
+++ Day14/main.py
@@ -0,0 +1,19 @@
+# Display art
+
+# Generate a random account from the game data
+
+# Format the account data into printable format
+
+# Ask user for a guess
+
+# Check if user is correct
+## Get follower count of each account
+## Use if statement to check if user is correct
+
+# Give user feedback based on their guess
+
+# Make the game repeatable
+
+# Making account at position B become the next account at position A
+
+# Clear the screen between rounds
