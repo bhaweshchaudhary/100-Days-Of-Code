a8190cbd89c92e8c315dd2b5563854fe1a0c723c
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-12-24 09:58:14 +0530

    Quiz project completed

--- Day17/data.py
+++ Day17/data.py
@@ -1,18 +1,34 @@
-question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.",
-     "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
-             "you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, "
-             "you are entitled to a state funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.",
-     "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+question_data = [{"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                  "question": "FLAC stands for Free Lossless Audio Condenser",
+                  "correct_answer": "False", "incorrect_answers": "True"},
+                 {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                  "question": "All program codes have to be compiled into an executable file in order to be run. This "
+                              "file can then be executed on any machine.",
+                  "correct_answer": "False", "incorrect_answers": "True"},
+                 {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                  "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source "
+                              "code in book form. ",
+                  "correct_answer": "True", "incorrect_answers": "False"},
+                 {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                  "question": "The open source program Redis is a relational database server.",
+                  "correct_answer": "False", "incorrect_answers": "True"},
+                 {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                  "question": "AMD created the first consumer 64-bit processor.",
+                  "correct_answer": "True", "incorrect_answers": "False"},
+                 {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                  "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+                  "correct_answer": "True", "incorrect_answers": "False"},
+                 {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                  "question": "The first dual-core CPU was the Intel Pentium D.",
+                  "correct_answer": "False", "incorrect_answers": "True"},
+                 {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                  "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+                  "correct_answer": "False", "incorrect_answers": "True"},
+                 {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                  "question": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, "
+                              "Android, and Chrome OS.",
+                  "correct_answer": "False", "incorrect_answers": "True"},
+                 {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                  "question": "Bhawesh Loves Coding?",
+                  "correct_answer": "True", "incorrect_answers": "False"}
 ]
--- Day17/main.py
+++ Day17/main.py
@@ -4,11 +4,15 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(text=question_text, answer=question_answer)
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
+
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've completed the quiz.\n")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
