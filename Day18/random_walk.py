9dfba80482385abe9d623ddc272dbe76ead4bdf2
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-12-25 10:10:24 +0530

    random walk with random rgb colors

--- Day18/random_walk.py
+++ Day18/random_walk.py
@@ -1,16 +1,24 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-panda = Turtle()
-colours = ["CornFlowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+panda = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_colors = (r, g, b)
+    return random_colors
+
+
 directions = [0, 90, 180, 270]
 panda.pensize(15)
 panda.speed("fastest")
 
 for _ in range(200):
-    panda.color(random.choice(colours))
+    panda.color(random_color())
     panda.forward(30)
     panda.setheading(random.choice(directions))
 
-mero_screen = Screen()
-mero_screen.exitonclick()
