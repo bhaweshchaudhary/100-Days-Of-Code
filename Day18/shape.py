8fdaa1d85aa3b141984c87a850f644b11304cdcf
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-12-25 08:23:52 +0530

    Created shape with turtle

--- Day18/shape.py
+++ Day18/shape.py
@@ -5,34 +5,42 @@ mero_turtle.shape("triangle")
 mero_turtle.color("green")
 
 for i in range(3):
+    mero_turtle.color("green")
     mero_turtle.forward(100)
     mero_turtle.right(120)
 
 for i in range(4):
+    mero_turtle.color("navy")
     mero_turtle.forward(100)
     mero_turtle.right(90)
 
 for i in range(5):
+    mero_turtle.color("red")
     mero_turtle.forward(100)
     mero_turtle.right(72)
 
 for i in range(6):
+    mero_turtle.color("maroon")
     mero_turtle.forward(100)
     mero_turtle.right(60)
 
 for i in range(8):
+    mero_turtle.color("deep pink")
     mero_turtle.forward(100)
     mero_turtle.right(45)
 
 for i in range(9):
+    mero_turtle.color("blue violet")
     mero_turtle.forward(100)
     mero_turtle.right(40)
 
 for i in range(10):
+    mero_turtle.color("orange red")
     mero_turtle.forward(100)
     mero_turtle.right(36)
 
 for i in range(12):
+    mero_turtle.color("linen")
     mero_turtle.forward(100)
     mero_turtle.right(30)
 
