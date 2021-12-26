51a53e1e477890af5ffe0ddae2fea56f9807620e
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-12-26 15:03:39 +0530

    breaking down the problem step by step

---
+++ Day20/main.py
@@ -0,0 +1,13 @@
+# Create a snake body
+
+# Move the snake
+
+# Control snake
+
+# Detect collision with food
+
+# Create a score board
+
+# Detect collision with wall
+
+# Detect collision with tail
