1208f06ae779018f3bcbd7cfa76e3e0097f980c1
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2021-12-28 11:07:06 +0530

    Finished snake game

---
+++ Day22/main.py
@@ -0,0 +1,8 @@
+#   1. Create a screen
+# 2. create and move the paddle
+# 3. create another paddle
+# 4. Create the ball and make it move
+# 5. Detect collision with wall and bounce
+# 6. Detect collision with paddle
+# 7. Detect when paddle misses
+# 8. Keep score.
