a132032c52b21bc8ebc8ee48765bd1bf900797af
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-01 17:16:43 +0530

    updated files in day 20-21 are copied in day 24 for modification

---
+++ Day24/main.py
@@ -0,0 +1,46 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+mero_screen = Screen()
+mero_screen.setup(width=600, height=600)
+mero_screen.bgcolor("black")
+mero_screen.title("Mero Snake Game")
+mero_screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+mero_screen.listen()
+mero_screen.onkey(snake.up, "Up")
+mero_screen.onkey(snake.down, "Down")
+mero_screen.onkey(snake.left, "Left")
+mero_screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    mero_screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 285 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -285:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+mero_screen.exitonclick()
