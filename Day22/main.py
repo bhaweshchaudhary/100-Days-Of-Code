c8c00eba538bd9e34a4482d79bfd1806f6ceaf4b
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-05 22:35:46 +0530

    Detect when right paddle misses the ball

--- Day22/main.py
+++ Day22/main.py
@@ -33,4 +33,9 @@ while game_is_on:
     # Detect collision with r_paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    # Detect R_Paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
 mero_screen.exitonclick()
