878299e7e2a5f92b334a0eb6ecac99a99157174a
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-05 23:48:17 +0530

    completed mero pong game

--- Day22/main.py
+++ Day22/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 mero_screen = Screen()
@@ -12,6 +13,7 @@ mero_screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 mero_screen.listen()
 mero_screen.onkey(r_paddle.go_up, "Up")
@@ -21,7 +23,7 @@ mero_screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     mero_screen.update()
     ball.move()
 
@@ -37,9 +39,11 @@ while game_is_on:
     # Detect R_Paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect L_Paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 mero_screen.exitonclick()
