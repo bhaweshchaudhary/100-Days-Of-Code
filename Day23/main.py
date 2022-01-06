4277413ce54a48bf8729c9f4bb7ffad4798fae48
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-07 01:54:41 +0530

    added scoreboard.py

--- Day23/main.py
+++ Day23/main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -20,13 +21,16 @@ while game_is_on:
     screen.update()
     car_manager.create_car()
     car_manager.move_cars()
+
     # Detect collision with the car
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
     # Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()
