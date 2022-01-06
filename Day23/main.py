ca18249c5cdb60cafb2c405ff7675d16f7315c2e
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-06 23:18:01 +0530

    ✅ Create and move the cars ✅ Detect collision with car

--- Day23/main.py
+++ Day23/main.py
@@ -9,8 +9,20 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+    # Detect collision with the car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
--- Day23/player.py
+++ Day23/player.py
@@ -12,3 +12,6 @@ class Player(Turtle):
         self.penup()
         self.goto(STARTING_POSITION)
         self.setheading(90)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
