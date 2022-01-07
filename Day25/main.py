4df086831930f7896d05b11aa92e5beb7cb79f6d
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-07 22:53:46 +0530

    changing monday temperature from fahrenheit to celsius.

--- Day25/main.py
+++ Day25/main.py
@@ -9,4 +9,13 @@ data = pandas.read_csv("2.1 weather_data.csv")
 
 # Get row of data with max temperature
 
-print(data[data.temp == data.temp.max()])
+max_temp_row = data[data.temp == data.temp.max()]
+
+# getting monday temperature in fahrenheit
+
+# getting hold of monday row
+
+monday = data[data.day == "Monday"]
+monday_temp = int(monday.temp)
+monday_temp_in_celsius = (monday_temp*1.8) + 32
+print(monday_temp_in_celsius)
