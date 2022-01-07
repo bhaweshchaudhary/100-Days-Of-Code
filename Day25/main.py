1cb3ae54cfe370a95a926cd4d099d1df4d2e1588
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-07 22:40:18 +0530

    getting row of data with max temp.

--- Day25/main.py
+++ Day25/main.py
@@ -1,2 +1,12 @@
 import pandas
 
+data = pandas.read_csv("2.1 weather_data.csv")
+# data_dict = data.to_dict()
+# data_list = data["temp"].to_list()
+# average_temp = data["temp"].mean()
+# max_value = data["temp"].max()
+# print(max_value)
+
+# Get row of data with max temperature
+
+print(data[data.temp == data.temp.max()])
