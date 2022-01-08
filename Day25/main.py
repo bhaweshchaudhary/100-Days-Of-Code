9aafdff3d0da95c3e67ae52635d620d8079b9bc5
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-08 23:07:10 +0530

    started final project

--- Day25/main.py
+++ Day25/main.py
@@ -29,3 +29,19 @@ import pandas
 # data = pandas.DataFrame(data_dict)
 # print(data)
 
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+total_gray_squirrel = len(data[data["Primary Fur Color"] == "Gray"])
+total_cinnamon_squirrel = len(data[data["Primary Fur Color"] == "Cinnamon"])
+total_black_squirrel = len(data[data["Primary Fur Color"] == "Black"])
+print(total_gray_squirrel)
+print(total_cinnamon_squirrel)
+print(total_black_squirrel)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Total Count": [total_gray_squirrel, total_cinnamon_squirrel, total_black_squirrel],
+}
+
+data_frame = pandas.DataFrame(data_dict)
+data_frame.to_csv("squirrel_count.csv")
+print(data_frame)
