6d2b44d1ce0b3ca506c4c981032cb5bf85880aba
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-09 22:23:21 +0530

    second exercise completed

---
+++ Day26/second_exercise.py
@@ -0,0 +1,9 @@
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above
+
+# Write your 1 line code 👇 below:
+result = [even for even in numbers if even % 2 == 0]
+
+# Write your code 👆 above:
+
+print(result)
