94a3a517c1ac3a08060101b965f73bc7594e2b11
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-11 15:28:15 +0530

    tuesday motivation code

--- Day32/main.py
+++ Day32/main.py
@@ -1,13 +1,19 @@
 import smtplib
+import datetime as dt
+import random
 
-my_email = "callme.bhawesh@yahoo.com"
-my_password = "jdeikinititlonlj"
+MY_EMAIL = "callme.bhawesh@yahoo.com"
+PASSWORD = "your_password"
 
-with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=my_password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="kaflekushal5@gmail.com",
-        msg="Subject:Testing\n\nThis is an automated python code to send emails."
-    )
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs="kaflekushal5@gmail.com",
+                            msg=f"Subject: Tuesday Motivation\n\n{quote}")
