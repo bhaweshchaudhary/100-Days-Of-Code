97475b22f796273c955b36d9b8a3e96f403b3a3a
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-13 13:02:34 +0530

    commenting code

--- Day45/main.py
+++ Day45/main.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 
-with open("website.html") as file:
-    content = file.read()
-
-soup = BeautifulSoup(content, 'html.parser')
-print(soup.title.string)
+# with open("website.html") as file:
+#     content = file.read()
+#
+# soup = BeautifulSoup(content, 'html.parser')
