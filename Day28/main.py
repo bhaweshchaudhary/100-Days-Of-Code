995a847ccb5c500b48c89ef4eef0f3422707646f
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-14 22:26:50 +0530

    working on pomodoro gui app

---
+++ Day28/main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50)
+canvas = Canvas(width=200, height=224)
+tamatar = PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=tamatar)
+canvas.create_text(103, 130, text="00:00")
+canvas.pack()
+
+window.mainloop()
