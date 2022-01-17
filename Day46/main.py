85f87ec28288181673d15742db734dcf86e29fcd
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-17 23:51:48 +0530

    day 29 file created

--- Day46/main.py
+++ Day46/main.py
@@ -1,13 +1,21 @@
-from date import Date
 import bs4
 import requests
 
-URL = "https://www.billboard.com/charts/hot-100/2000-08-12/"
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n")
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+soup = bs4.BeautifulSoup(response.text, "html.parser")
+song_title = soup.find_all("li", class_="o-chart-results-list__item")
+songs = [song.getText() for song in song_title]
+print(songs)
 
-# mero_date = Date()
-response = requests.get(URL)
-mero_soup = response.text
-soup = bs4.BeautifulSoup(mero_soup, "html.parser")
-
-song_name = soup.find_all(name="h3", id="title-of-a-story").text
-print(song_name)
+# from bs4 import BeautifulSoup
+# import requests
+#
+# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+#
+# response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+#
+# soup = BeautifulSoup(response.text, 'html.parser')
+# song_names_spans = soup.find_all("span", class_="chart-element__information__song")
+# song_names = [song.getText() for song in song_names_spans]
+# print(song_names)
