32c57f80f950957f9e6034058ea271829be3158e
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-21 12:29:29 +0530

    raising error in bmi code if human height entered is more than 3 meters.

--- Day30/main.py
+++ Day30/main.py
@@ -1,34 +1,43 @@
-# FileNotFound Error
-# with open("a_file.txt") as file:
-#   file.read()
+# # FileNotFound Error
+# # with open("a_file.txt") as file:
+# #   file.read()
+#
+# # KeyError
+# # a_dictionary = {"key":"value"}
+# # value = a_dictionary["non_existence_key"]
+#
+# # IndexError
+# # fruit_list = ["apple", "orange", "pineapple"]
+# # fruit = fruit_list[3]
+#
+# # TypeError
+# # text = 'abc'
+# # print(text + 5)
+#
+# # FileNotFound Error (handling error)
+# try:
+#     file = open("a.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     # print("there was an error")
+#     file = open("a.txt", "w")
+#     file.write("something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exists.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     # file.close()
+#     # print("File was closed.")
+#     raise KeyError("this is an error that i made up.")
 
-# KeyError
-# a_dictionary = {"key":"value"}
-# value = a_dictionary["non_existence_key"]
 
-# IndexError
-# fruit_list = ["apple", "orange", "pineapple"]
-# fruit = fruit_list[3]
+height = float(input("Height: "))
+weight = float(input("Weight: "))
 
-# TypeError
-# text = 'abc'
-# print(text + 5)
+if height > 3:
+    raise ValueError("Human height should not be more than 3 meters.")
 
-# FileNotFound Error (handling error)
-try:
-    file = open("a.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    # print("there was an error")
-    file = open("a.txt", "w")
-    file.write("something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exists.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    # file.close()
-    # print("File was closed.")
-    raise KeyError("this is an error that i made up.")
+bmi = weight / height ** 2
