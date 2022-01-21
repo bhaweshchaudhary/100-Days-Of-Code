a0227e67be3b96683ce271b128dfab56c50bae7c
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-21 13:23:02 +0530

    Index Error Exercise

---
+++ Day30/indexerrorexercise.py
@@ -0,0 +1,13 @@
+fruits = ["Apple", "Pear", "Orange"]
+
+
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit Pie")
+    else:
+        print(fruit + " pie")
+
+
+make_pie(4)
