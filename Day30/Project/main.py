aab1f6f793f958e0cc0c0e6d7f4513ac8430896c
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-22 09:59:34 +0530

    handling errors and exception in nato phonetic alphabet project

--- Day30/Project/main.py
+++ Day30/Project/main.py
@@ -9,11 +9,14 @@ code = reading_csv.code
 code_list = code.to_list()
 
 mero_dic = {letter_list[i]: code_list[i] for i in range(len(letter_list))}
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [mero_dic[letter] for letter in word]
+    except KeyError:
+        print("Only Letters in the alphabets are acceptable.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-word = input("Enter a word: ").upper()
-try:
-    output_list = [mero_dic[letter] for letter in word]
-except KeyError:
-    print("One Letters are acceptable.")
-else:
-    print(output_list)
+generate_phonetic()
