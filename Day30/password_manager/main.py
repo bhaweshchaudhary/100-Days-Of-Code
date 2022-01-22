9f85f2730c35dbe7df1a47fcfcfb8a85cffadafd
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-01-22 13:44:40 +0530

    completed password manager

--- Day30/password_manager/main.py
+++ Day30/password_manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 
 #   ----------------------------- PASSWORD GENERATOR -----------------------#
@@ -33,27 +34,50 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {website: {
+        "email": email,
+        "password": password,
+    }}
 
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty.")
     else:
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                # saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
-        is_ok = messagebox.askokcancel(title="Password Confirmation", message=f"These are the"
-                                                                              f"details entered:\n"
-                                                                              f"Website: {website}\n"
-                                                                              f"Email: {email}\nPassword:"
-                                                                              f" {password}\n"
-                                                                              f"Is it okay to save?\n")
 
-        if is_ok:
-            with open("data.txt", "a") as passwords:
-                passwords.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                email_entry.delete(0, END)
-                password_entry.delete(0, END)
+#   ----------------------------- FIND PASSWORD -----------------------#
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exist")
 
 
-#   ----------------------------- UI SETUP -----------------------#
+#  ----------------------------- UI SETUP -----------------------#
 
 window = Tk()
 window.title("Password Generator")
@@ -74,10 +98,10 @@ password_label = Label(text="Password")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-website_entry.insert(0, "callmebhawesh.github.io")
+website_entry.insert(0, "Amazon")
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "thisisbhawesh@gmail.com")
@@ -85,6 +109,8 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
