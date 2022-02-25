465f7fe8be516d12d1e968c5a21bbe3e99065da6
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-02-25 09:22:58 +0530

    mini update

--- Day 36/counter/word_counter/views.py
+++ Day 36/counter/word_counter/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def counter(request):
+    text = request.GET['text']
+    text_count = text.split()
+    text_counted = len(text_count)
+    return render(request, 'counter.html', {text_counted: 'text'})
