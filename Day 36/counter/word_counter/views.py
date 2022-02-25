1afd69a5c90ccd74f6e1c70e75f14d13e2c02cf1
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-02-25 10:29:42 +0530

    updated

--- Day 36/counter/word_counter/views.py
+++ Day 36/counter/word_counter/views.py
@@ -1,10 +1,9 @@
-from multiprocessing import context
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
 
 def counter(request):
-    words = request.GET['text']
+    words = request.POST['text']
     text_count = len(words.split())
-    return render(request, 'counter.html', {'text': text_count})
+    return render(request, 'counter.html', {"text": text_count})
