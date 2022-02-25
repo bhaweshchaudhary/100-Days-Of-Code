516ea7c7052839ae23dd34734a7fb3e225dd9035
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-02-25 07:17:05 +0530

    update

--- Day 36/counter/counter/settings.py
+++ Day 36/counter/counter/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'word_counter.apps.WordCounterConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'counter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
