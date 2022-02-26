ba12ebbbfdbe9780e73590cfe1311ac0b112037d
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-02-26 14:11:32 +0530

    update

--- Day 37/appland/app/models.py
+++ Day 37/appland/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Header(models.Model):
+    head = models.CharField(max_length=100)
+    main_text = models.CharField(max_length=100)
+    desc = models.CharField(max_length=100)
+
+class SecondSection(models.Model):
+    head = models.CharField(max_length=100)
--- Day 37/appland/app/views.py
+++ Day 37/appland/app/views.py
@@ -1,8 +1,13 @@
+from email.header import Header
 from django.shortcuts import render, HttpResponse
+from .models import Header, SecondSection
 
 # Create your views here.
 def app(request):
-    head = "App Land"
-    main_text = "Promote Your App with App Land"
-    desc = "We can promote your App better than anyone."
-    return render(request, 'index.html', {"logo": head, "main_text": main_text, "desc": desc,})
+    my_header = Header()
+    second = SecondSection()
+    my_header.head = "App Land"
+    my_header.main_text = "Promote Your App with App Land"
+    my_header.desc = "We can promote your App better than anyone."
+    second.head = "Save your time using App Land"
+    return render(request, 'index.html', {"logo": my_header.head, "main_text": my_header.main_text, "desc": my_header.desc, "second_head": second.head})
