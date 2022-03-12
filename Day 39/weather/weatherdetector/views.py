9b481e3ceced513f020f69dd614d19771a3984a0
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-03-12 10:55:29 +0530

    weather detection

---
+++ Day 39/weather/weatherdetector/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import json
+import urllib.request
+
+# Create your views here.
+def index(request):
+    if request.method == "POST":
+        city = request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=34b6b2f1188380f6864093be2f718303').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon'])+' '+str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp'])+'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+    else:
+        city = ''
+        data = {}
+    return render(request, 'index.html', {'city':city, 'data': data})
