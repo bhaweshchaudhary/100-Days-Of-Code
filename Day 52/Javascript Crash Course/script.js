9112135687742a4e43ad27e7c9b7d4b9c7c996e1
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-03 01:24:13 +0530

    js

--- Day 52/Javascript Crash Course/script.js
+++ Day 52/Javascript Crash Course/script.js
@@ -1,1 +1,23 @@
-document.getElementById("screen").innerHTML = "hi"
+let seven = document.getElementById("seven");
+let four = document.getElementById("four");
+let one = document.getElementById("one");
+let decimal = document.getElementById("decimal");
+let xsquare = document.getElementById("xsquare");
+let eight = document.getElementById("eight");
+let five = document.getElementById("five");
+let two = document.getElementById("two");
+let zero = document.getElementById("zero");
+let percentage = document.getElementById("percentage");
+let nine = document.getElementById("nine");
+let six = document.getElementById("six");
+let three = document.getElementById("three");
+let del = document.getElementById("del");
+let divide = document.getElementById("divide");
+let multiply = document.getElementById("multiply");
+let add = document.getElementById("add");
+let equalsto = document.getElementById("equalsto");
+
+function pi() {
+    let pi = document.getElementById("pi");
+    document.getElementById("screen").innerHTML = pi;
+}
