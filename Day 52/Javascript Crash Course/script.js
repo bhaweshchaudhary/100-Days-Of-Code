cbb47d79bc04c1bea1c1feed95514ed970263062
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-02 23:21:52 +0530

    hmm

--- Day 52/Javascript Crash Course/script.js
+++ Day 52/Javascript Crash Course/script.js
@@ -1,8 +1,1 @@
-// alert("This is js")
-// console.log('hey')
-
-// let hey = prompt("Enter any number\n");
-// document.getElementById("heading").innerHTML = hey;
-
-// Numbers in Javascript
-
+document.getElementById("screen").innerHTML = "hi"
