e223f363d5d29920a2c471b7b72772b8e333bdfd
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-06 18:09:25 +0530

    bahubali flask

--- Day 54/app.py
+++ Day 54/app.py
@@ -39,6 +39,21 @@ def delete(sno):
     db.session.commit()
     return redirect('/')
 
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
