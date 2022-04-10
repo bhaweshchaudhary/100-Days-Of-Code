5fc4a63d4cd2df11be39f3b1585608ca1bb283a4
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-10 12:39:59 +0530

    update

--- Day 55/app.py
+++ Day 55/app.py
@@ -3,5 +3,5 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def homepage():
     return render_template("index.html")
---
+++ Day 56/Day 1/traverse.c
@@ -0,0 +1,8 @@
+//    operation on Array Data Structure
+//    1. Traversal
+
+//    algorithm
+//    Step 1: Take user input for the size of the array
+//    Step 2: take the content to store in that array from user input
+//    step 3: print the content one by one
+
