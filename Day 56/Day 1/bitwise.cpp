bbe60cf18f745f7974833b34388f39b525efabc0
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-12 13:39:54 +0530

    bitwise OR Operator

--- Day 56/Day 1/bitwise.cpp
+++ Day 56/Day 1/bitwise.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
     int a = 5;
     int b = 7;
-    cout << ("a & b = ") << (a&b) << endl;
+    cout << ("a | b = ") << (a|b) << endl;
+    // cout << ("a & b = ") << (a&b) << endl;
 }
