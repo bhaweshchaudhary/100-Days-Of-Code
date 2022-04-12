a24f03b1ac741be070cec9300ea53f1970a93148
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-12 21:26:32 +0530

    data type

---
+++ Day 57/one.java
@@ -0,0 +1,10 @@
+public class one{
+    public static void main(String[] args){
+        byte age = 32;
+        int number = 1234567890;
+        long phoneNumber = 12345678900L;
+        float pi = 3.14F;
+        char letter = '@';
+        boolean isAdult = true;
+    }
+}
