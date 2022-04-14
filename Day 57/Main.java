265c808269bc8a803d306f71640e792c00184e9a
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-14 17:00:59 +0530

    java taba

---
+++ Day 57/Main.java
@@ -0,0 +1,115 @@
+// simple java program
+
+// public class one{
+//     public static void main(String[] args){
+//         System.out.println("Hello World");
+//     }
+// }
+
+
+//  boolean data type
+
+// public class one{
+//     public static void main(String[] args){
+//         int mark = 78;
+//         boolean isPass = false;
+//         if(mark>40){
+//             isPass = true;
+//         }
+//         System.out.println(isPass);
+//     }
+// }
+
+//byte data type in java
+
+// public class one{
+//     public static void main(String[] args){
+//         byte num = 32;
+//         System.out.println(num);
+//     }
+// }
+
+
+// Char data type in java
+
+// public class Main{
+//     public static void main(String args[]){
+//         char example = 97;
+//         System.out.println(example);
+//     }
+// }
+
+
+// short 
+
+// public class Main{
+//     public static void main(String args[]){
+//         short test = 32111; //  if i store 321111 then it'll throw error
+//         System.out.println(test);
+//     }
+// }
+
+
+// int
+
+// public class Main{
+//     public static void main(String args[]){
+//         int number = 1234567890; // it will throw error if i store 12345678901
+//         System.out.println(number);
+//     }
+// }
+
+// long
+
+// public class Main{
+//     public static void main(String args[]){
+//         long number = 1234567890111213141L; // if i try to store 12345678901112131415L then it will throw an error.
+//         System.out.println(number);
+
+//     }
+// }
+
+
+// float
+
+// public class Main{
+//     public static void main(String[] args) {
+//         float number = 1.12345675555F;
+//         System.out.println(number);
+//     }
+// }
+
+// double
+
+// public class Main{
+//     public static void main(String[] args) {
+//         double number = 1.1234567552255566D;
+//         System.out.println(number);
+//     }
+// }
+
+
+// String
+
+// public class Main{
+//     public static void main(String args[]){
+//         String question = new String("What is your name?");
+//         System.out.println(question);
+//         String answer = "My name is Bhawesh.";
+//         System.out.println(answer);
+//     }
+// }
+
+
+// String concatenation
+
+// public class Main{
+//     public static void main(String[] args){
+//         String firstName = new String("Bhawesh ");
+//         String lastName = new String("Chaudhary.");
+//         String fullName = new String(firstName + lastName);
+//         System.out.println(fullName);
+//     }
+// }
+
+
