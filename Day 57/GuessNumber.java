16418c27ada65eccafb291d16c05079a2c53cdd5
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-15 23:34:36 +0530

    commented out

--- Day 57/GuessNumber.java
+++ Day 57/GuessNumber.java
@@ -1,38 +1,38 @@
-import java.util.Random;
-import java.util.Scanner;
+// import java.util.Random;
+// import java.util.Scanner;
 
-public class GuessNumber {
+// public class GuessNumber {
 
-    private static void checkInput(int takingInput, int randomNumber){
-        if(takingInput!=randomNumber){
-            Scanner takeMyInput = new Scanner(System.in); // CREATING SCANNER OBJECT
-            System.out.println("Guess a number between 1 and 100"); // ASKING USER TO GUESS
-            takingInput = takeMyInput.nextInt(); // TAKING USER INPUT
-        }
+//     private static void checkInput(int takingInput, int randomNumber){
+//         if(takingInput!=randomNumber){
+//             Scanner takeMyInput = new Scanner(System.in); // CREATING SCANNER OBJECT
+//             System.out.println("Guess a number between 1 and 100"); // ASKING USER TO GUESS
+//             takingInput = takeMyInput.nextInt(); // TAKING USER INPUT
+//         }
 
-        if(takingInput==randomNumber){
-            System.out.println("congrats number matched.");
-        }
-    }
-    public static void main(String args[]){
-        Random randomObject = new Random(); // CREATING RANDOM OBJECT
-        int randomNumber = randomObject.nextInt(101); // GENERATING AND STORING RANDOM NUMBER
-        System.out.println("random generated number is "+ randomNumber);
-        Scanner takeMyInput = new Scanner(System.in); // CREATING SCANNER OBJECT
-        System.out.println("Guess a number between 1 and 100"); // ASKING USER TO GUESS
-        int takingInput = takeMyInput.nextInt(); // TAKING USER INPUT
+//         if(takingInput==randomNumber){
+//             System.out.println("congrats number matched.");
+//         }
+//     }
+//     public static void main(String args[]){
+//         Random randomObject = new Random(); // CREATING RANDOM OBJECT
+//         int randomNumber = randomObject.nextInt(101); // GENERATING AND STORING RANDOM NUMBER
+//         System.out.println("random generated number is "+ randomNumber);
+//         Scanner takeMyInput = new Scanner(System.in); // CREATING SCANNER OBJECT
+//         System.out.println("Guess a number between 1 and 100"); // ASKING USER TO GUESS
+//         int takingInput = takeMyInput.nextInt(); // TAKING USER INPUT
 
-        boolean notExit = true; // BOOLEAN HELPS IN TERMINATING WHILE LOOP
-        while(notExit){
-            checkInput(takingInput, randomNumber);
-            notExit = false;
+//         boolean notExit = true; // BOOLEAN HELPS IN TERMINATING WHILE LOOP
+//         while(notExit){
+//             checkInput(takingInput, randomNumber);
+//             notExit = false;
 
-        }
+//         }
 
-        System.out.println("You guessed " + takingInput);
+//         System.out.println("You guessed " + takingInput);
 
-    }
-}
+//     }
+// }
 
 
 
@@ -45,3 +45,6 @@ ALGORITHM
 4. INSIDE, CALL A FUNCTION, IF RANDOM NUMBER != USER INPUT, THEN ASK AGAIN FOR USER INPUT.
 5. IF RANDOM NUMBER = USER INPUT, THEN WHILE LOOP TERMINATES.
 */
+
+
+
