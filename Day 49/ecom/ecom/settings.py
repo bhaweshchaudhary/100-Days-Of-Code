fa7397d9af33a82bb5e5ee43f974d1f1b204d62d
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-16 10:55:45 +0530

    few changes

--- Day 49/ecom/ecom/settings.py
+++ Day 49/ecom/ecom/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!v(a%h03=gy*t3y+*nbs(yxc39cm)9*1s-*oy))=8@4f^^!o69'
+# SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
