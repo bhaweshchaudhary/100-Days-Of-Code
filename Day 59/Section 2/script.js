bfa2d592dc3846d7cbe5c5bb01e9a55d74a5a910
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-17 17:22:49 +0530

    hmm

---
+++ Day 59/Section 2/script.js
@@ -0,0 +1,86 @@
+let js = "amazing";
+// if (js === "amazing") alert("js is fun");
+console.log(23 + 12 - 40);
+console.log("Bhawesh");
+
+let fName = "Bhawesh";
+let lName = "Chaudhary";
+
+
+// Naming convention
+// 1. camel case notation is standard eg. myFirstNameIsBhaweshChaudhary
+// 2. illegal variable eg. 3years; bhawesh&chadhary
+// 3. Js only contain letters, numbers, underscore, or dollar sign
+// 4. don't use reserved javascript keyword such as new, this, 
+// 5. don't start variable with upper case for normal variable, only use it for constant variables.
+// 6. variable name should be descriptive, means, easy to read and understand.
+
+// Assignment 1 
+
+let country = 'Nepal';
+let continent = 'South Asia';
+let population = '2.91 Crores';
+
+console.log("The continent of " + country + " is " + continent + " and it's population is " + population);
+
+
+// Data Types
+
+// 7 data types
+
+// Number  ->  for numbers of any kind: integer or floating-point, integers are limited by ±(253-1).
+
+
+// string -> for strings. A string may have zero or more characters, there’s no separate single-character type.
+
+
+// let fullName = fName + " " + lName;
+// console.log(fullName);
+// console.log(`the result of 4+6 is ${4 + 6}`)
+
+
+
+// boolean -> for true/false.
+
+
+
+// undefined (variable without value) -> for unassigned values – a standalone type that has a single value undefined.
+
+// let number;
+// console.log(number)
+
+
+// Null -> for unknown values – a standalone type that has a single value null.
+// let age = null;
+// console.log(age)
+
+
+// Symbol -> for unique identifiers.
+
+
+
+// BigInt -> is for integer numbers of arbitrary length.
+
+
+// object -> for more complex data structures.
+
+
+// the typeof operator
+
+// console.log(typeof undefined);       // returns undefined
+// console.log(typeof alert);          // return function
+// console.log(typeof 10n);            // returns bigint
+// console.log(typeof true);           // returns boolean
+// console.log(typeof 1);              // returns number
+// console.log(typeof "id");           // returns string
+// console.log(typeof Symbol("id"));   // returns symbol
+// console.log(typeof Math);           // returns object
+// console.log(typeof null);           // returns object
+
+
+// alert, prompt, and confirm
+
+// let age = prompt("What is your age ?")
+// alert(`You are ${age} yrs old.`)
+
+
