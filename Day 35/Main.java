f12032196e6485c640fe2a847d1eff3e1506ccd4
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-05-01 19:25:42 +0530

    simple example of oops in java

--- Day 35/Main.java
+++ Day 35/Main.java
@@ -1,5 +1,19 @@
-class Main{
+class Pen{
+    String color;
+    String type;
+
+    void printSomething(){
+        System.out.println(this.color);
+    }
+}
+
+public class Main{
     public static void main(String args[]){
-        arr
+        Pen pen1 = new Pen();
+        Pen pen2 = new Pen();
+        pen1.color = "Blue";
+        pen2.color = "Black";
+        pen1.printSomething();
+        pen2.printSomething();
     }
 }
