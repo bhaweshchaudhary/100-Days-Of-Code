fdc79df57fbedff1c4dd7aaa31f75081207ea1c7
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-05-02 08:14:39 +0530

    first question done

--- Day 35/Main.java
+++ Day 35/Main.java
@@ -1,19 +1,28 @@
-class Pen{
-    String color;
-    String type;
+// Write a program to create a class Student with data ‘name, city and age’ along with method printData to display the data. Create the two objects s1 ,s2 to declare and access the values. 
 
-    void printSomething(){
-        System.out.println(this.color);
+class Student{
+    String name;
+    String city;
+    int age;
+
+    void printData(){
+        System.out.println("Name is "+ this.name);
+        System.out.println("City is "+ this.city);
+        System.out.println("Age is "+ this.age + "\n");
     }
 }
 
 public class Main{
     public static void main(String args[]){
-        Pen pen1 = new Pen();
-        Pen pen2 = new Pen();
-        pen1.color = "Blue";
-        pen2.color = "Black";
-        pen1.printSomething();
-        pen2.printSomething();
+        Student s1 = new Student();
+        s1.name = "Bhawesh";
+        s1.city = "Vizag";
+        s1.age = 21;
+        Student s2 = new Student();
+        s2.name = "Einstein";
+        s2.city = "Zurich";
+        s2.age = 199;
+        s1.printData();
+        s2.printData();
     }
 }
