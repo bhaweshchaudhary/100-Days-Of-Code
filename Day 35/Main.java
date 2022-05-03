90a2ea095b50f0e4ca235b053421778fc24ef520
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-05-03 15:28:28 +0530

    solid rectangle pattern in java

--- Day 35/Main.java
+++ Day 35/Main.java
@@ -1,28 +1,34 @@
 // Write a program to create a class Student with data ‘name, city and age’ along with method printData to display the data. Create the two objects s1 ,s2 to declare and access the values. 
 
-class Student{
+class Student {
     String name;
     String city;
     int age;
 
+    public Student(String name, String city, int age) {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+    }
+// copy constructor
+    Student(Student s2){
+        name = s2.name;
+        city = s2.city;
+        age = s2.age;
+    }
+
     void printData(){
-        System.out.println("Name is "+ this.name);
-        System.out.println("City is "+ this.city);
-        System.out.println("Age is "+ this.age + "\n");
+        System.out.println(this.name);
+        System.out.println(this.city);
+        System.out.println(this.age);
     }
 }
 
-public class Main{
-    public static void main(String args[]){
-        Student s1 = new Student();
-        s1.name = "Bhawesh";
-        s1.city = "Vizag";
-        s1.age = 21;
-        Student s2 = new Student();
-        s2.name = "Einstein";
-        s2.city = "Zurich";
-        s2.age = 199;
-        s1.printData();
+public class Main {
+    public static void main(String args[]) {
+        Student s2 = new Student("bhawesh", "vizag", 19);
         s2.printData();
+
     }
 }
+
---
+++ Day 35/SolidRectangle.java
@@ -0,0 +1,16 @@
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+
+public class SolidRectangle{
+    public static void main(String args[]){
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=5; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
