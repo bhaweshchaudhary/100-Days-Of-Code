1a615f6b644b14b7129028d87bd50bb512364af4
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-05-11 23:04:08 +0530

    modification

--- Day 35/script.js
+++ Day 35/script.js
@@ -1,42 +1,36 @@
 // initialize the variable
+
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 
 let songs = [
-    {
-        songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"
-    },
-    {
-        songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"
-    },
-    {
-        songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"
-    },
-    {
-        songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"
-    },
-    {
-        songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"
-    },
-    {
-        songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"
-    },
-    {
-        songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"
-    },
-    {
-        songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"
-    }
+    {songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"},
+    {songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"},
+    {songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"},
+    {songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"},
+    {songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"},
+    {songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"},
+    {songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"},
+    {songName: "despacito", filepath: "songs/1.mp3", coverpath: "cover/1.jpg"},
 ]
 
 // audioElement.play();
 
 // Handle play/pause click
-masterPlay.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime>=0){
+
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
         audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-circle-pause');
+    }
+    else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-play-circle');
     }
 })
 
@@ -45,5 +39,4 @@ masterPlay.addEventListener('click', ()=>{
 myProgressBar.addEventListener('timeupdate', () => {
     console.log('timeupdate');
     // update seekbar
-
 });
