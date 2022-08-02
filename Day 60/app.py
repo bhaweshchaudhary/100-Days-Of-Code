5cb807d6ae1ac5977736a22d68594b3d4fbd01be
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-08-02 16:28:56 +0530

    Day\ 60

--- Day 60/app.py
+++ Day 60/app.py
@@ -7,13 +7,5 @@ app = Flask(__name__, template_folder="template")
 def home():
     return render_template("index.html")
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello, {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin"))
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
