e2f6fffba593eca6335f95f15bd6297c16f48b8a
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-08-31 21:09:56 +0530

    MOD

--- Day 65/gfg/authentication/urls.py
+++ Day 65/gfg/authentication/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
 ]
--- Day 65/gfg/authentication/views.py
+++ Day 65/gfg/authentication/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("hello")
+    return render(request, "authentication/index.html")
+
+def signup(request):
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    pass
