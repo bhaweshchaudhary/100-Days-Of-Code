fd433078b13d9e8f6e48ae53df958c8194b800f5
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-10-01 15:27:34 +0530

    server socket method

--- Day 67/main.py
+++ Day 67/main.py
@@ -1,6 +1,11 @@
 import socket
 
-# socket.socket(socket_family, socket_type, protocol=0)
+# s = socket.socket(socket_family, socket_type, protocol=0)
 # socket family is either Af_UNIX or AF_INET
 # socket type is either SOCK_STREAM or SOCK_DGRAM
 # PROTOCOL default is 0
+
+# server socket method
+# s.bind() -> bind address (hostname, port number pair) to socket.
+# s.listen() -> sets up and start TCP listener.
+# s.accept() => this passively accept TCP client connection, wait until connection arrives (blocking).
